112ff86455e8722b70b85edfc96ece8fdf09169c
spencerrfree <sf376@byu.edu>
2023-01-17 19:08:30 -0700

    Loop

--- SpencerFreeMission2/Program.cs
+++ SpencerFreeMission2/Program.cs
@@ -6,12 +6,38 @@ using System.Threading.Tasks;
 
 namespace SpencerFreeMission2
 {
+    public class PrintStuff
+    {
+        public void PrintNumRolls(double numRolls)
+        {
+            Console.WriteLine("Total number of rolls = " + numRolls +".");
+        }
+    }
     public class Program
     {
-        //Start
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintStuff myGame = new PrintStuff();
+            double numRolls = 0;
+            string str = "";
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many dice rolls would you like to simulate ? ");
+            str = Console.ReadLine();
+            numRolls = Convert.ToDouble(str);
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each '*' represents 1 % of the total number of rolls.");
+            myGame.PrintNumRolls(numRolls);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = rnd.Next(1, 7);
+                int dice2 = rnd.Next(1, 7);
+                int sum = dice1 + dice2;
+                Console.WriteLine(sum);
+            }
+            
         }
     }
 }
